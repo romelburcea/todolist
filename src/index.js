dc419b8fa182be3f4f711d615c452ae09d527cd1
Romel Burcea <romel.burcea@gmail.com>
2024-11-18 23:16:18 +0200

    "added more to the projects object"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { format, compareAsc } from 'date-fns';
 import "./styles.css";
 
+
+const projectsList  = document.querySelector(".projectsList")
+
+const projects = [new todo("Clean", "Dishes, Vacuum", "19/11/2024", "Medium", "Need to wash dishes and what not", "Done")]
+
 function todo (title, description, dueDate, priority, notes, checklist){
     this.title = title
     this.description = description
@@ -23,3 +28,21 @@ addbtn.addEventListener("click", () => {
 cancel.addEventListener("click", () => {
     dialog.close()
 })
+
+
+function showProjects(){
+    for(let i = 0; i < projects.length; i++){
+        const newproject = document.createElement("div")
+        newproject.classList.add(".newproject")
+        newproject.innerHTML = `<div class="book">
+                <h4>Title: ${projects[i].title} </h4>
+                <h4>Description: ${projects[i].description} </h4>
+                <h4>Due Date: ${projects[i].dueDate}</h4>
+                <h4>Priority: ${projects[i].priority}</h4>
+                <h4>Notes: ${projects[i].notes}</h4>
+                <h4>Done</h4> ${project[i].checklist}`
+
+            projectsList.appendChild(newproject)
+    }
+
+}
