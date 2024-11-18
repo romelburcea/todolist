3d43075ecd0e3252ab9d9fad83d1f3ecc270bb60
Romel Burcea <romel.burcea@gmail.com>
2024-11-18 20:08:44 +0200

    did more stuff

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import { format, compareAsc } from 'date-fns';
 import "./styles.css";
+
+function todo (title, description, dueDate, priority, notes, checklist){
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+    this.notes = notes
+    this.checklist = checklist
+}
+
 
